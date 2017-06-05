b7f651c86e7446f6c62aa8e75b55e06748546941
Christian Mauderer <oss@c-mauderer.de>
2017-06-05 13:51:56 +0200

    build: Update build-libbsd with version from GRiSP.

--- build/build-libbsd.sh
+++ build/build-libbsd.sh
@@ -15,24 +15,58 @@ export PATH="${PREFIX}/bin:${PATH}"
 
 cd "${LIBBSD_SOURCE_DIR}"
 
-# ugly workaround to build the tests with external RAM
-if [ ! -e "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org" ]
+if [ "$BSP_NAME" == "atsamv" ]
 then
-	mv "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds" "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org"
+	# ugly workaround to build the tests with external RAM
+	if [ ! -e "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org" ]
+	then
+		mv "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds" \
+		    "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org"
+	fi
+	cp "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.sdram" \
+	    "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds"
 fi
-cp "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.sdram" "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds"
+
+# Evaluate options
+DO_CLEAN=0
+DO_INSTALL=0
+for i in "$@" ; do
+	case "$i" in
+	clean)
+		DO_CLEAN=1
+		;;
+	install)
+		DO_INSTALL=1
+		;;
+	*)
+		echo "Unexpected option: '$i'"
+		exit 1
+		;;
+	esac
+done
 
 # Build and install libbsd
-if [ -e wscript ]
+if [ $DO_CLEAN -ne 0 ]
 then
-    waf clean
+	if [ -e build ]
+	then
+		waf clean
+	fi
 fi
 
 waf configure \
 	--prefix="${PREFIX}" \
 	--rtems-bsps="${RTEMS_CPU}/${BSP_NAME}"
 waf
-waf install
 
-# part 2 of ugly workaround to build the tests with external RAM
-mv "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org" "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds"
+if [ $DO_INSTALL -ne 0 ]
+then
+	waf install
+fi
+
+if [ "$BSP_NAME" == "atsamv" ]
+then
+	# part 2 of ugly workaround to build the tests with external RAM
+	mv "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds.org" \
+	    "${PREFIX}/${TARGET}/${BSP_NAME}/lib/linkcmds"
+fi
