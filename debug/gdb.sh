87163a0e34d7d1a7d5ebdede131610cf5dbc89e8
Christian Mauderer <oss@c-mauderer.de>
2017-07-04 20:06:40 +0200

    debug: Find out path.

--- debug/gdb.sh
+++ debug/gdb.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-../rtems-install/rtems-4.12/bin/arm-rtems4.12-gdb -x start.gdb $@
+# find out own directory
+SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+PROJECTDIR="${SCRIPTDIR}/../"
+
+${PROJECTDIR}/rtems-install/rtems-4.12/bin/arm-rtems4.12-gdb -x ${SCRIPTDIR}/start.gdb $@
